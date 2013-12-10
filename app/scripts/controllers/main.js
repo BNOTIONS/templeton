e6ba2fa8cfb78658342abc1d6e8dce4c0a0ff7be
Jonathon W. Marshall <jonathon@bnotions.com>
2013-12-10 10:32:50 -0500

    initial commit of yo angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('templetonApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
